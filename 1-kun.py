31b3260bc4a82ea61476deaf8703488d11f16aa0
axrorback <axacyber1@gmail.com>
2025-01-23 21:46:26 +0500

    Python Darslari

---
+++ 1-kun.py
@@ -0,0 +1,39 @@
+#strings
+mening_ismim = "Axrorjon"
+ovqat = "Osh"
+pochta = "axacyber1@gmail.com"
+
+#Integers
+
+yoshim = 20
+
+miqdor  =   5
+
+print(f"Men {yoshim} yoshdaman")
+print(f"Men bugun {miqdor} ta non sotib oldim")
+
+talabalar_soni  =   28
+
+print(f"Bizning guruhda {talabalar_soni} ta talaba bor")
+
+#float
+
+narx = 10.5
+print(f"Mahsulotning narxi {narx} so'm")
+gpa = 4.9
+print(f"Mening gpa: {gpa} ball teng ")
+
+
+#Boolean
+
+talaba = True
+if talaba:
+    print("Tabriklayman siz Talabasiz")
+else:
+    print("Afsus siz talaba emassiz")
+
+mahsulot = False
+if mahsulot:
+    print("Mahsulot sotuvda mavjud")
+else:
+    print("Mahsulot Sotuvda mavjud emas!!!")
---
+++ 2-kun.py
@@ -0,0 +1,15 @@
+#2-kun
+#Typecasting
+"""ism = "Ahrorjon"
+yosh = 20
+gpa = 3.2
+talaba = True
+yosh = float(yosh)
+ism = bool(ism)
+print(ism)
+"""
+#input() function
+ism = input("What is your name:")
+print(f"Salom {ism} yaxshimisiz?")
+yosh = input("Necha yoshdasiz?:")
+print(f"Siz {yosh} yoshdasiz ")
