ef742a11c464ac1e95e0e4abcf605407f2c95334
Ahrorjon Ibrohimjonov <89008689+softwareuz@users.noreply.github.com>
2025-01-24 17:30:29 +0500

    3-kun.py

--- 3-kun.py
+++ 3-kun.py
@@ -1,13 +1,13 @@
 #3-kun
 #exercise 2 Shopping Cart Program
-""""" mahsulot = input("Qanday mahsulot sotib olishni hohlaysiz?:")
+mahsulot = input("Qanday mahsulot sotib olishni hohlaysiz?:")
 narx = float(input("Narxi qancha?:"))
 dona = int(input("Necha dona sotib olishni hohlaysiz?:"))
 umumiy = narx * dona
 print(f"Siz sotib olyapsiz {dona} x {mahsulot}")
 print(f"Umumiy narx {umumiy} so'mga teng!")
-"""
-"""#Madlibs Game
+
+#Madlibs Game
 #word game where you create a story
 #by filling in blanks with random words
 adjective1 = input("Enter an adjactive(Description):")
@@ -20,4 +20,4 @@ print(f"Today I wnet to a {adjective1} zoo.")
 print(f"In an exhibit, I saw a {noun1}")
 print(f"{noun1} was {adjective2} and {verb1}")
 print(f"I was {adjective3}!")
-"""
+
