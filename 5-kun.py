79b77677803b60410a93795a770d5b6205abbad6
axrorback <axacyber1@gmail.com>
2025-01-25 01:26:50 +0500

    5 kun Darslari

--- 5-kun.py
+++ 5-kun.py
@@ -1,5 +1,5 @@
 #5-kun  if  else elif = else if
-"""
+
 age = int(input("Enter your age:"))
 if age >=100:
     print("You are soo told to Sign Up!")
@@ -9,18 +9,18 @@ elif age >= 18:
     print("You are now signed Up!")
 else:
     print("You must be 18+ to Sign Up!")
-"""
-"""
+
+
 response = input("Would you like food (Y/N):")
 if response == "Y" or "y":
     print("Have some food!")
 else:
     print("No food for you !")
-"""
-"""
+
+
 name = input("Enter your name:")
 if name == "":
     print("You did not enter your name!")
 else:
     print(f"Welcome {name} our system")
-"""
+
