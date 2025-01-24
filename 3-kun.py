4a42b0e53e9c52fd22da80d5f96b7844e593d326
axrorback <axacyber1@gmail.com>
2025-01-24 17:16:44 +0500

    Python Darslari

---
+++ 3-kun.py
@@ -0,0 +1,22 @@
+#3-kun
+#exercise 2 Shopping Cart Program
+""""" mahsulot = input("Qanday mahsulot sotib olishni hohlaysiz?:")
+narx = float(input("Narxi qancha?:"))
+dona = int(input("Necha dona sotib olishni hohlaysiz?:"))
+umumiy = narx * dona
+print(f"Siz sotib olyapsiz {dona} x {mahsulot}")
+print(f"Umumiy narx {umumiy} so'mga teng!")
+"""
+#Madlibs Game
+#word game where you create a story
+#by filling in blanks with random words
+adjective1 = input("Enter an adjactive(Description):")
+noun1 = input("Enter a noun(Person ,place, thing):")
+adjective2 = input("Enter an adjactive(Description):")
+verb1 = input("Enter a verb ending with ing:")
+adjective3 = input("Enter an adjactive(Description):")
+
+print(f"Today I wnet to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
