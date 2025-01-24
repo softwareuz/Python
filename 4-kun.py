fb85e95eaeb7292df2de0227232d8536a0858bd3
axrorback <axacyber1@gmail.com>
2025-01-25 00:44:09 +0500

    4 kun Darslari

---
+++ 4-kun.py
@@ -0,0 +1,17 @@
+#4-kun
+import math
+print(math.pi)
+print(math.e)
+x = int(input("Enter number for calculate root:"))
+result = math.sqrt(x)
+print(f"Root is {result}")
+#Find Length
+
+radius = float(input("Enter the radius:"))
+c = 2*math.pi*radius
+print(f"The area is {c} ")
+
+a = float(input("Enter side a:"))
+b = float(input("Enter side b:"))
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+print(f"c:{c}")
