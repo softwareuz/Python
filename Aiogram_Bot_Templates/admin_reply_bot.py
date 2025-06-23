1d5f3ffaa833e0c5413cd3ee1fe1934d4d7e99b4
Ahrorjon Ibrohimjonov <89008689+softwareuz@users.noreply.github.com>
2025-06-24 00:35:12 +0500

    Update admin_reply_bot.py

--- Aiogram_Bot_Templates/admin_reply_bot.py
+++ Aiogram_Bot_Templates/admin_reply_bot.py
@@ -8,8 +8,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.storage.memory import MemoryStorage
 
 # Bot tokeningiz
-API_TOKEN = "7524411808:AAH_dwkkUdzZbMCyuXxwSJ0UWpvTEC-J_QA"
-ADMIN_ID = 5789956459  # Admin ID
+API_TOKEN = #Token
+ADMIN_ID =   # Admin ID
 
 # Logging yoqish
 logging.basicConfig(level=logging.INFO)
